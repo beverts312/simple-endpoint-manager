5526c6f28fd80a866005fb7508dc41e630370a11
beverts312 <texas312@gmail.com>
2018-05-29 21:48:57 -0600

    feat: support path routes

--- README.md
+++ README.md
@@ -2,5 +2,15 @@
 Leverage Consul and Nginx to easily router traffic
 
 ## How to use it
-Certs - Put certs `/security` named `public.pem` and `private.pem`.  
-Routes - Create routes in consul KV using the pattern `domain/subdomain` : `target`.
+Configure DNS for all the domains you want to the place where the nginx container is running
+
+Tip: Use wildcard subdomains if you plan on using a bunch
+
+#### Certs  
+Put certs `/security` named `public.pem` and `private.pem`.
+All traffic on `80` will be redirected to `443`.
+
+You can get free certificates from [ZeroSSL](https://zerossl.com/).
+
+#### Routes  
+Create routes in consul with the source being the key and target being the value.
--- img/app/generate.py
+++ img/app/generate.py
@@ -4,11 +4,11 @@ import json, sys, re
 jsonFile = sys.argv[1]
 with file(jsonFile) as f: jsonText = f.read()
 
-jsonText = re.sub(",(\s*)}", r"\1}", jsonText)
+domains = json.loads(re.sub(",(\s*)}", r"\1}", jsonText))
+oneLocationDomains = filter(lambda a: "/" not in a[0], domains.iteritems())
+multiLocationDomains = filter(lambda a: "/" in a[0], domains.iteritems())
 
-domains = json.loads(jsonText)
-
-for sourceUrl, targetUrl in domains.iteritems():
+for sourceUrl, targetUrl in oneLocationDomains:
   print """
     server {
       listen 443 ssl;
@@ -22,3 +22,27 @@ for sourceUrl, targetUrl in domains.iteritems():
       }
     }
   """ % (sourceUrl, targetUrl)
+
+mappedDomains = {}
+for sourceUrl, targetUrl in multiLocationDomains:
+  domain = sourceUrl.split('/')[0]
+  path = sourceUrl.replace(domain, '')
+  if domain not in mappedDomains: mappedDomains[domain] = {}
+  mappedDomains[domain][path] = targetUrl
+
+for domain in mappedDomains:
+  print """
+   server {
+      listen 443 ssl;
+      server_name %s; 
+  """ % domain
+  for path in mappedDomains[domain]:
+    print """
+     location %s {
+        proxy_set_header      X-Real-IP $remote_addr;
+        proxy_ssl_server_name on;
+        proxy_ssl_name        $proxy_host;
+        proxy_pass            %s;
+      }
+    }
+  """ % (path, mappedDomains[domain][path])
